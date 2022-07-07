d2de91c672d8b792deba12e245f67299a4a2bfa3
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-07 19:16:17 +0530

    n=int(input()) sum=0 oc=0 arr=list(map(int,input().split())) for i in range(0,n):     c=1     for j in range(0,n):         if arr[i]!=-1:             if arr[i]==arr[j] and i!=j:                 c+=1                 arr[i]=-1     if c==1:         if arr[i]%2!=0:             oc+=1 print(oc)

---
+++ odd_unique_elements_count.py
@@ -0,0 +1,15 @@
+n=int(input())
+sum=0
+oc=0
+arr=list(map(int,input().split()))
+for i in range(0,n):
+    c=1
+    for j in range(0,n):
+        if arr[i]!=-1:
+            if arr[i]==arr[j] and i!=j:
+                c+=1
+                arr[i]=-1
+    if c==1:
+        if arr[i]%2!=0:
+            oc+=1
+print(oc)
