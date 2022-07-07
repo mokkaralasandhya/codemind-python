03db731828dd46b12dda11acd28321ea98546891
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-07 19:24:53 +0530

    n=int(input()) arr=list(map(int,input().split())) a,b=map(int,input().split()) min=999 c=0 for i in range(n):     if arr[i]>=a and arr[i]<=b:         c+=1         if arr[i]<min:             min=arr[i] if c==0:     print(-1) else:     print(min)

---
+++ minimum_between_A_and_B.py
@@ -0,0 +1,14 @@
+n=int(input())
+arr=list(map(int,input().split()))
+a,b=map(int,input().split())
+min=999
+c=0
+for i in range(n):
+    if arr[i]>=a and arr[i]<=b:
+        c+=1
+        if arr[i]<min:
+            min=arr[i]
+if c==0:
+    print(-1)
+else:
+    print(min)
