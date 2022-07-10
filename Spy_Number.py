25c991b488b3ebbedf31dfbf17a1978ad7595537
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-10 21:15:11 +0530

    n=int(input()) s=0;p=1; while(n!=0):     r=n%10     s+=r     p*=r     n//=10 if s==p:     print("Spy Number") else:     print("Not Spy Number")

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,13 +1,11 @@
 n=int(input())
-temp=n
-sum=0
-p=1
-while temp>0:
-    lastdigit=temp%10
-    sum+=lastdigit
-    p*=lastdigit
-    temp//=10
-if sum==p:
+s=0;p=1;
+while(n!=0):
+    r=n%10
+    s+=r
+    p*=r
+    n//=10
+if s==p:
     print("Spy Number")
 else:
     print("Not Spy Number")
