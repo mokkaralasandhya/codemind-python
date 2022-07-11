d4056eea6a3fa1f60ab9385057b9cbaa9128ffd9
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-11 11:19:58 +0530

    def sod(a):     su=0     for i in range(1,a):         if a%i==0:             su+=i     return su a=int(input()) b=int(input()) if sod(a)==b and sod(b)==a:     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,12 @@
+def sod(a):
+    su=0
+    for i in range(1,a):
+        if a%i==0:
+            su+=i
+    return su
 a=int(input())
 b=int(input())
-s1=0
-s2=0
-for i in range(1,a):
-    if a%i==0:
-        s1+=i
-for i in range(1,b):
-    if b%i==0:
-        s2+=i
-if(s1==b and s2==a):
+if sod(a)==b and sod(b)==a:
     print("Amicable")
 else:
     print("Not Amicable")
