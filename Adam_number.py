79d8bc56a2eface26015bde3b266cf991d3cf913
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-07-19 07:15:26 +0530

    n=int(input()) r=0 sp=n*n t=0 s=0 while(n>0):     r=r*10+n%10     n=n//10 sq=r*r while(sq>0):     t=t*10+sq%10     sq//=10 if(sp==t):     print("True") else:     print("False")

---
+++ Adam_number.py
@@ -0,0 +1,16 @@
+n=int(input())
+r=0
+sp=n*n
+t=0
+s=0
+while(n>0):
+    r=r*10+n%10
+    n=n//10
+sq=r*r
+while(sq>0):
+    t=t*10+sq%10
+    sq//=10
+if(sp==t):
+    print("True")
+else:
+    print("False")
