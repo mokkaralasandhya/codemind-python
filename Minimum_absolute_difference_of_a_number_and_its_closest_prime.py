e03773d1852df78b3e8213e58c695ffc856f7ced
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-05 19:37:40 +0530

    N=int(input()) for i in range(N,2,-1):     for j in range(2,i):         if i%j==0:             #print(i)             break     else:         break for m in range(N+1,N+100):     for n in range(2,m):         if m%n==0:             #print(i)             break     else:         break if abs(N-m)>=abs(N-i):     print(abs(N-i)) else:     print(abs(N-m))

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,33 +1,19 @@
-x=int(input())
-d=x
-c=0
-for i in range(1,x+1):
-    if x%i==0:
-        c+=1
-if c==2:
-    print("0")
-else:
-    for i in range(x,0,-1):
-        c=0
-        for j in range(1,i+1):
-            if i%j==0:
-                c+=1
-        if c==2:
-            min=i
+N=int(input())
+for i in range(N,2,-1):
+    for j in range(2,i):
+        if i%j==0:
+            #print(i)
             break
-    while True:
-        c=0
-        for j in range(1,x+1):
-            if x%j==0:
-                c+=1
-        if c==2:
-            max=x
-            break
-        x+=1
-    r1=d-min
-    r2=max-d
-    if r1<r2:
-        print(r1)
     else:
-        print(r2)
-    
+        break
+for m in range(N+1,N+100):
+    for n in range(2,m):
+        if m%n==0:
+            #print(i)
+            break
+    else:
+        break
+if abs(N-m)>=abs(N-i):
+    print(abs(N-i))
+else:
+    print(abs(N-m))
