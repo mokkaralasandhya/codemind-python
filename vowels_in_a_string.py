cc200f7aa4180fdf5f2e901d980143abe6646437
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-06 22:15:51 +0530

    a=input() s=input() if s in a:     print(True) for i in range(len(a)):     if s in a[i]:         print(i)         break else:     print(False)

--- vowels_in_a_string.py
+++ vowels_in_a_string.py
@@ -1,13 +1,10 @@
-n=input()
+a=input()
 s=input()
-c=0
-for i in range(len(n)):
-    if n[i]==s[0]:
-        c+=1
-        d=i
+if s in a:
+    print(True)
+for i in range(len(a)):
+    if s in a[i]:
+        print(i)
         break
-if c>0:
-    print("True")
-    print(d)
 else:
-    print("False")
+    print(False)
