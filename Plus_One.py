aee27acba387d97bad136cf75a00ca1f4c512c5f
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-07 22:23:29 +0530

    n=int(input()) a=list(map(int,input().split())) r=0 c=0 for i in range(n):     r=r*10+a[i] r+=1 t=r while(t):     t//=10     c+=1 if(c>n):     n+=1     a.append(0) while(r):     d=r%10     a[n-1]=d     r//=10     n-=1 print(*a)

---
+++ Plus_One.py
@@ -0,0 +1,20 @@
+n=int(input())
+a=list(map(int,input().split()))
+r=0
+c=0
+for i in range(n):
+    r=r*10+a[i]
+r+=1
+t=r
+while(t):
+    t//=10
+    c+=1
+if(c>n):
+    n+=1
+    a.append(0)
+while(r):
+    d=r%10
+    a[n-1]=d
+    r//=10
+    n-=1
+print(*a)
