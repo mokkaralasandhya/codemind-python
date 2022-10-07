5ac76b1af3923cc774030d83f646157f64cab67c
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-07 09:31:24 +0530

    s=input() s=s.lower() k=s[::-1] a='aeiouAEIOU' c=0 for i in range(len(s)//2):     if s[i] in a and k[i] not in a:         if s[i]!=' ':             if k[i]!=' ':                 c+=1     elif s[i] not in a and k[i] in a:         if s[i]!=' ':             if k[i]!=' ':                 c+=1 print(c)

---
+++ count_vowel_consonent_pairs.py
@@ -0,0 +1,15 @@
+s=input()
+s=s.lower()
+k=s[::-1]
+a='aeiouAEIOU'
+c=0
+for i in range(len(s)//2):
+    if s[i] in a and k[i] not in a:
+        if s[i]!=' ':
+            if k[i]!=' ':
+                c+=1
+    elif s[i] not in a and k[i] in a:
+        if s[i]!=' ':
+            if k[i]!=' ':
+                c+=1
+print(c)
