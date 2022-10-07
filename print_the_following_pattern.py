e9246e23ef58a5cf0e9e8fe0b82aebad79061041
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-07 18:32:02 +0530

    rows=int(input()) for i in range(rows,0,-1):     for j in range(1,i+1):         print(chr(i+64),end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,5 @@
-a=int(input())
-for i in range(a):
-    for j in range(i+1):
-        print(j+1,end="")
+rows=int(input())
+for i in range(rows,0,-1):
+    for j in range(1,i+1):
+        print(chr(i+64),end=" ")
     print()
