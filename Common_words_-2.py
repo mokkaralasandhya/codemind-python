3e245789c0f30201c5f63a69f0e8778c34742268
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-07 13:11:17 +0530

    s=input() s=s.lower() s1=input() s1=s1.lower() s=s.split() s1=s1.split() c=0 for i in s:     if s.count(i)==1 and i in s1:         if s1.count(i)==1:             c+=1 print(c)

---
+++ Common_words_-2.py
@@ -0,0 +1,12 @@
+s=input()
+s=s.lower()
+s1=input()
+s1=s1.lower()
+s=s.split()
+s1=s1.split()
+c=0
+for i in s:
+    if s.count(i)==1 and i in s1:
+        if s1.count(i)==1:
+            c+=1
+print(c)
