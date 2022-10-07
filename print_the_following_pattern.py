71f0ce88a38606555b984de7e62c6cd5d5077eb1
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-07 18:20:21 +0530

    a=int(input()) for i in range(a):     for j in range(a):         print(chr(i+65),end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,5 @@
 a=int(input())
-for i in range(1,a+1):
-    for j in range(1,a+1):
-        if(i==j or j==a-i+1):
-            print('x',end="")
-        else:
-            print('0',end="")
+for i in range(a):
+    for j in range(a):
+        print(chr(i+65),end=" ")
     print()
