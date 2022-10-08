28401f2c64e9419a764f83563e6bbed5554f7ab6
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-08 11:40:20 +0530

    def fun(n):     c=0     while(n):         r=n%10         c+=1         n//=10     return c a=int(input()) x=list(map(int,input().split())) f=[] for i in x:     f.append(fun(i)) m=max(f) print(f.count(m))

--- Max_digits_count.py
+++ Max_digits_count.py
@@ -1,12 +1,14 @@
-a=int(input())
-b=input()
-arr=list(b.split())
-mi=0
-for i in arr:
-    if len(i)>mi:
-        mi=len(i)
-c=0
-for i in arr:
-    if len(i)==mi:
+def fun(n):
+    c=0
+    while(n):
+        r=n%10
         c+=1
-print(c)
+        n//=10
+    return c
+a=int(input())
+x=list(map(int,input().split()))
+f=[]
+for i in x:
+    f.append(fun(i))
+m=max(f)
+print(f.count(m))
