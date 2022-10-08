64c1c73cb8c1f1bc95c6f6373cf093692c26db53
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-08 12:14:42 +0530

    def fun(n):     s=0     while(n>0):         r=n%10         s=s*10+r         n//=10     return s n=int(input()) a=list(map(int,input().split())) for i in a:     print(fun(i),end=' ')

--- Reverse_game.py
+++ Reverse_game.py
@@ -1,11 +1,11 @@
 def fun(n):
     s=0
-    while(n):
+    while(n>0):
         r=n%10
         s=s*10+r
         n//=10
     return s
 n=int(input())
-b=list(map(int,input().split()))
-for s in b:
-    print(fun(s),end=' ')
+a=list(map(int,input().split()))
+for i in a:
+    print(fun(i),end=' ')
