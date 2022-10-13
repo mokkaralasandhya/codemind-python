3e822d36eb4d9a9e96f6f4e48aa8ee3abfb6c88b
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-13 19:41:03 +0530

    s=input() s=s.lower() s=s.split() c=0 m=0 k=s[0] for i in k:     for j in s:         if i in j:             continue         else:             break     else:         m=1         print(i,end='') if m==0:     print(-1)

--- Common_characters_-1.py
+++ Common_characters_-1.py
@@ -1,25 +1,18 @@
-a=input()
-a2=""
-res=""
-for i in a:
-    if i.isupper():
-        a2+=i.lower()
+s=input()
+s=s.lower()
+s=s.split()
+c=0
+m=0
+k=s[0]
+for i in k:
+    for j in s:
+        if i in j:
+            continue
+        else:
+            break
     else:
-        a2+=i
-arr=list(a2.split())
-for i in arr:
-    for j in i:
-        f=0
-        for k in arr:
-            if j in k:
-                f=1
-            else:
-                f=0
-                break
-        if f==1:
-            res+=j
-    break
-if len(res)==0:
+        m=1
+        print(i,end='')
+if m==0:
     print(-1)
-else:
-    print(res)
+        
