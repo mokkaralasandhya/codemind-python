4d23342e75a4409d1288595fa50ab8c219aeff5c
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-13 20:41:22 +0530

    a=input() b=[] for i in range(len(a)):     if a[i] not in b:         b.append(a[i])         c=b for i in c:     if i in 'aeiouAEIOU':         print(i,end=' ')

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,13 +1,9 @@
 a=input()
-vow="aeiouAEIOU"
-c=0
-arr=[]
-for i in a:
-    if i in vow:
-        if i not in arr:
-            arr.append(i)
-if len(arr)==0:
-    print(-1)
-else:
-    for i in arr:
-        print(i,end=" ")
+b=[]
+for i in range(len(a)):
+    if a[i] not in b:
+        b.append(a[i])
+        c=b
+for i in c:
+    if i in 'aeiouAEIOU':
+        print(i,end=' ')
