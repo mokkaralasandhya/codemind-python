e98ed13cceaf9c5ef27cf5d2ab96756e4c365d45
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-13 22:11:52 +0530

    n,x=map(int,input().split()) s=0 for j in range(n):     m=list(map(int,input().split()))     for i in range(len(m)):         if j==0 or j==(n-1):             s=s+m[i]         else:             if i==0 or i==(len(m)-1):                 s=s+m[i] print(s)

--- Sum_of_all_boundary_values_in_a_matrix.py
+++ Sum_of_all_boundary_values_in_a_matrix.py
@@ -4,8 +4,8 @@ for j in range(n):
     m=list(map(int,input().split()))
     for i in range(len(m)):
         if j==0 or j==(n-1):
-            s+=m[i]
+            s=s+m[i]
         else:
             if i==0 or i==(len(m)-1):
-                s+=m[i]
+                s=s+m[i]
 print(s)
