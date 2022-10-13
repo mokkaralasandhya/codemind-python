d08a004a39614fc9d408d36bc567c811ace070d8
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-13 20:15:54 +0530

    s1=input() s2=input()
    for i in s1:
        if i in s2:
            print(True)
            break
        else:
            print(False)
            break

--- Anagrams.py
+++ Anagrams.py
@@ -1,15 +1,11 @@
-a=input()
-b=input()
-f=0
-if len(a)==len(b):
-    for i in a:
-        if i in b or i.upper() in b or i.lower() in b:
-            f=1
-        else:
-            f=0
-    if f==1:
-        print("True")
+s1=input()
+s2=input()
+
+for i in s1:
+    if i in s2:
+        print(True)
+        break
     else:
-        print("False")
-else:
-    print("False")
+        print(False)
+        break
+        
