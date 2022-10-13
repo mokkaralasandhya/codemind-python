040ff5e230335df8ff495f3e65c2c1e1072f69d1
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-13 21:54:27 +0530

    s=input() s=s.lower() k=s[::-1] a='aeiouAEUIO' c=0 for i in range((len(s)//2)):     if s[i] in a and k[i] not in a:         if s[i]!=' ':             if k[i]!=' ':               c+=1     elif s[i] not in a and k[i] in a:         if s[i]!=' ':             if k[i]!=' ':               c+=1 print(c)

--- count_vowel_consonent_pairs.py
+++ count_vowel_consonent_pairs.py
@@ -1,15 +1,15 @@
 s=input()
 s=s.lower()
 k=s[::-1]
-a='aeiouAEIOU'
+a='aeiouAEUIO'
 c=0
-for i in range(len(s)//2):
+for i in range((len(s)//2)):
     if s[i] in a and k[i] not in a:
         if s[i]!=' ':
             if k[i]!=' ':
-                c+=1
+              c+=1
     elif s[i] not in a and k[i] in a:
         if s[i]!=' ':
             if k[i]!=' ':
-                c+=1
-print(c)
+              c+=1
+print(c)        
