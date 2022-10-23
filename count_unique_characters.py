0791badabc129cbe882ae4f09d7e535a825891d4
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-23 20:33:03 +0530

    s=input() s=s.lower() a=[] b=[] for i in range(len(s)):     if s[i]==' ':         continue     if s.count(s[i])==1:         a.append(s[i]) print(len(a))

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,11 +1,10 @@
-n=input()
-n=n.lower()
-c=0
-for i in range(len(n)):
-    s=0
-    for j in range(len(n)):
-        if n[i]==n[j] and n[i]!=' ':
-            s+=1
-    if s==1:
-        c+=1
-print(c)
+s=input()
+s=s.lower()
+a=[]
+b=[]
+for i in range(len(s)):
+    if s[i]==' ':
+        continue
+    if s.count(s[i])==1:
+        a.append(s[i])
+print(len(a))
