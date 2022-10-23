fa45b08908bac225cbb0c68c24c6b1a8776d2f4a
mokkaralasandhya <97589202+mokkaralasandhya@users.noreply.github.com>
2022-10-23 19:47:55 +0530

    n=int(input()) k=n r=0 while(k):     d=k%10     r=r*10+d     k//=10 b=r+n r2=0 while(b!=r2):     m=b     r2=0     while(m):         d=m%10         r2=r2*10+d         m//=10     if(r2!=b):b+=r2 print(r2)

---
+++ Reverse_Palindrome.py
@@ -0,0 +1,18 @@
+n=int(input())
+k=n
+r=0
+while(k):
+    d=k%10
+    r=r*10+d
+    k//=10
+b=r+n
+r2=0
+while(b!=r2):
+    m=b
+    r2=0
+    while(m):
+        d=m%10
+        r2=r2*10+d
+        m//=10
+    if(r2!=b):b+=r2
+print(r2)
